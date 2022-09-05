5e7bc3cdada33d5ca7c5ac14afda7c98cc950a92
Cem Tanrikut <cemtanrikut@gmail.com>
2022-09-05 20:10:01 +0300

    Added icons

--- blank-app/src/App.js
+++ blank-app/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import logo from './logo_profile.PNG';
 import './App.css';
+import instagram from './pngInstagram.png';
+import twitter from './pngTwitter.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo ml25" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <code>Yellow Umbrella Studios | Coming soon...</code>
+        </p>  
+        <header className="social-header">
+          <a
+              className="App-link"
+              href="https://instagram.com/yellowumbrellagames"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instagram} className="social-logo" alt="logo" ></img>
+            </a>
+            <a
+              className="App-link"
+              href="https://twitter.com/yugamestudios"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitter} className="social-logo ml25" alt="logo" ></img>
+            </a>
+        </header>
       </header>
     </div>
   );
